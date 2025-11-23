802cd57f4208c7f73a5945ceeb50acfd6ee2953b
Natalie Gagnon <nataliegagnon444@gmail.com>
2025-11-22 21:01:08 -0600

    new manager store [id] page +layout

---
+++ app/manager/stores/[id]/layout.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ManagerStoresLayout({ children }) {
+  return <>{children}</>;
+}
--- app/manager/stores/layout.tsx
+++
@@ -1,6 +0,0 @@
-// app/manager/stores/layout.tsx
-"use client";
-
-export default function ManagerStoresLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
