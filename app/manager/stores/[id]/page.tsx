450d22400ce6babda177bc9b2bb03918d1c25a10
Natalie Gagnon <nataliegagnon444@gmail.com>
2025-11-22 21:20:32 -0600

    fix pages

--- app/manager/stores/[id]/page.tsx
+++ app/manager/stores/[id]/page.tsx
@@ -1,5 +1,13 @@
 "use client";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 import { useParams } from "next/navigation";
+
+// TEMP HACK:
+console.log("STORE PAGE BOOTING");
+
 
 export default function StorePage() {
   const { id } = useParams();
--- app/manager/stores/page.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-export default function StoresRoot() {
-  return <div style={{ padding: 40 }}>STORES ROOT PAGE</div>;
-}
